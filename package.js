21075af8ef59b2466f75b9c2d3beabd22d01c1aa
A. Fritz <me@a-fritz.com>
2013-09-04 14:18:19 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
 	summary: "aviary image editor repackaged for Meteor"
 });
-
+ Npm.depends({
+  "imagemagick":"https://github.com/and-fri/node-imagemagick.git"
+});
 Package.on_use(function (api) {
 	api.add_files([
 		'feather.js'
